33f9a458daeb33fda0f611c5024d0cd069addc4d
Saqib Shaikh <me@saqibshaikh.com>
2012-05-05 18:07:25 +0100

    Adding initial version of source code

---
+++ NP/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace NP
+{
+    using System;
+    using System.Diagnostics;
+    using System.IO;
+
+    internal static class Program
+    {
+        internal static void Main(string[] args)
+        {
+            try
+            {
+                string[] filesToOpen;
+                if (args.Length == 0)
+                {
+                    string text = Console.In.ReadToEnd();
+                    string tempFilename = Path.GetTempFileName();
+                    File.WriteAllText(tempFilename, text);
+                    filesToOpen = new string[] { tempFilename };
+                }
+                else
+                {
+                    filesToOpen = args;
+                }
+
+                string notepadPath = Path.Combine(Environment.SystemDirectory, "notepad.exe");
+                foreach (string filename in filesToOpen)
+                {
+                    Process.Start(notepadPath, filename);
+                }
+            }
+            catch (Exception x)
+            {
+                Console.Error.WriteLine("Error: {0}", x.Message);
+            }
+        }
+    }
+}
